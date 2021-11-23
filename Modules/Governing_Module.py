5c9b63045c78c3967a8503005ae61de4064e2f11
G3tLUCky <56052135+G3tLUCky@users.noreply.github.com>
2021-11-24 00:04:22 +0100

    Update Governing_Module.py

--- Modules/Governing_Module.py
+++ Modules/Governing_Module.py
@@ -6,8 +6,10 @@ from Template_Filler_Module import Template_Filler
 from Text_Collection_Module import Text_Collection
 from Visual_Display_Module import Visual_Display
 
-os.chdir("C:/Users/lucla/Documents/Studie/Tilburg University/Masterscriptie")
-path = "C:/Users/lucla/Documents/Studie/Tilburg University/Masterscriptie/databases/Cijfers/XML_files"
+current_path = os.getcwd()
+new_path = current_path + "\databases"
+os.chdir(new_path)
+
 leg, temp = Template_Converter("Templates_complete.xlsx")
 data = Data_Files(path)
 
@@ -19,7 +21,6 @@ templates = Template_Filler(gapped_templates, data, idx)
 text = Text_Collection(templates)
 Visual_Display(data, data[idx-6:idx+1])
 
-os.chdir("C:/Users/lucla/Documents/Studie/Tilburg University/Masterscriptie/Output")
 file = open("Covid_Text.txt", 'w', encoding = "UTF-8")
 file.write(text)
 file.close()
